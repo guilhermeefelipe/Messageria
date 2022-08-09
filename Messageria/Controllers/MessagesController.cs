425f3ebc4298318b357cf7f29e60fbbc71080de8
guilh <guilh@GUILHERME>
2022-08-09 11:20:39 -0300

    Adicionar arquivos de projeto.

---
+++ Messageria/Controllers/MessagesController.cs
@@ -0,0 +1,63 @@
+﻿using Messageria.InputModels;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+
+namespace Messageria.Controllers
+{
+    [Route("api/messages")]
+    public class MessagesController : ControllerBase
+    {
+
+        private const string QUEUE_NAME = "messages";
+        private readonly ConnectionFactory _factory;
+
+        public MessagesController()
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage([FromBody] SendMessageInputModel sendMessageInputModel)
+        {
+            using (var connection = _factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    // Declara Fila para que caso ela nao exista ainda, ela seja criada
+                    channel.QueueDeclare(
+                         queue: QUEUE_NAME,
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null
+                        );
+
+                    // Formatar os dados para envio para a fila
+                    var stringMessage = JsonSerializer.Serialize(sendMessageInputModel);
+                    var byteArray = Encoding.UTF8.GetBytes(stringMessage);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: QUEUE_NAME,
+                        basicProperties: null,
+                        body: byteArray
+                        );
+                }
+            }
+
+            return Accepted();
+        }
+    }
+
+}
---
+++ Messageria/InputModels/SendMessageInputModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Messageria.InputModels
+{
+    public class SendMessageInputModel
+    {
+        public string FromUser { get; set; }
+        public string ToUser { get; set; }
+        public string Content  { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
